d335922fce8134d4de02a882b225e6f6309333a6
Magnus Aagaard Sørensen <mgsdk@users.noreply.github.com>
2016-12-11 17:00:31 +0100

    Comment on amplitude for echo.

--- Matlab/echom.m
+++ Matlab/echom.m
@@ -6,13 +6,10 @@ clc
 [x, Fs] = audioread('hello.wav');
 
 % The number of unit step delay.
-d = ceil(Fs / 256);
-
-d
-d / Fs
+d = ceil(Fs / 1);
 
 % The strenght of the echo.
-alpha = 1;
+alpha = 1 / 32
 
 % Generate FIR filter.
 b = [1, zeros(1, d), alpha];
